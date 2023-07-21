f3e229dd1a2ef7203ddc143e0921567d18f0a402
13virat <viratgupta1314@gmail.com>
2023-07-22 01:48:09 +0530

    ff

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import { UserButton } from "@clerk/nextjs";
+
 import Image from "next/image";
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard Page (Protected)</p>
-      <UserButton afterSignOutUrl="/" />
+      
     </div>
   );
 };
